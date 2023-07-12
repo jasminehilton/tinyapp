b4eb61ad0a4c5c596b2fdd439f94e5717291cd7f
jasminehilton <jasminehilton1@live.com>
2023-07-11 21:12:02 -0700

    Added ability to store cookie same as username

--- express_server.js
+++ express_server.js
@@ -68,6 +68,12 @@ app.post("/urls/:id", (req, res) => {
   res.redirect("/urls");
 });
 
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  res.cookie("username", username);
+  res.redirect("/urls");
+});
+
 const generateRandomString = function () {
   return (+new Date() * Math.random()).toString(36).substring(0, 6);
 };
