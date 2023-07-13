a15d62a363e394ee6c35915bce05dd1a8524a7ea
jasminehilton <jasminehilton1@live.com>
2023-07-13 14:58:02 -0700

    permissions created

--- express_server.js
+++ express_server.js
@@ -11,10 +11,22 @@ app.set("view engine", "ejs");
 
 //============================================= GLOBAL CONST
 
+// const urlDatabase = {
+//   b2xVn2: "http://www.lighthouselabs.ca",
+//   "9sm5xK": "http://www.google.com",
+// };
+
 const urlDatabase = {
-  b2xVn2: "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com",
+  b2xVn2: {
+    longURL: "http://www.lighthouselabs.ca",
+    userID: "userRandomID",
+  },
+  Psm5xK: {
+    longURL:  "http://www.google.com",
+    userID:  "user2RandomID",
+  },
 };
+
 const users = {
   userRandomID: {
     id: "userRandomID",
@@ -33,6 +45,16 @@ const generateRandomString = function() {
   return (+new Date() * Math.random()).toString(36).substring(0, 6);
 };
 
+const urlsForUser = function(userID) {
+  let userUrls = {};
+  for (let shortURL in urlDatabase) {
+    if (urlDatabase[shortURL].userID === userID) {
+      userUrls[shortURL] = urlDatabase[shortURL];
+    }
+  }
+  return userUrls;
+};
+
 //============================================= LISTEN
 
 app.listen(PORT, () => {
@@ -42,85 +64,115 @@ app.listen(PORT, () => {
 //============================================= GET
 
 app.get("/", (req, res) => {
-  res.send("Hello!");
+  return res.send("Hello!");
 });
 
 app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
+  return res.json(urlDatabase);
 });
 
 app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+  return res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 app.get("/urls", (req, res) => {
+  const userID = req.cookies["user_id"];
+  const userURLs = urlsForUser(userID);
   const templateVars = {
-    urls: urlDatabase,
+    urls: userURLs,                               
     email: req.cookies["email"],
   };
-  res.render("urls_index", templateVars);
+  if (!templateVars.email) {
+    return res.redirect("/login");
+  }
+  return res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
   const templateVars = {
     email: req.cookies["email"],
   };
-  res.render("urls_new", templateVars);
+  if (!templateVars.email) {
+    return res.redirect("/login");
+  }
+
+  return res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
   const templateVars = {
     id: req.params.id,
-    longURL: urlDatabase[req.params.id],
+    longURL: urlDatabase[req.params.id].longURL,
+    email: req.cookies["email"]
   };
-  res.render("urls_show", templateVars);
+  if (!templateVars.email) {
+    return res.send(401);
+  }
+
+  return res.render("urls_show", templateVars);
 });
 
 app.get("/u/:id", (req, res) => {
-  const longURL = urlDatabase[req.params.id];
-  res.redirect(longURL);
+  const longURL = urlDatabase[req.params.id].longURL;                               
+  if (!longURL) {                               
+    return res.status(404).send("This shortened url does not exist");
+  }
+
+  return res.redirect(longURL);
 });
 
 app.get("/register", (req, res) => {
   const templateVars = {
     email: req.cookies["email"],
   };
-  res.render("_signup", templateVars);
+  return res.render("_signup", templateVars);
 });
 
 app.get("/login", (req, res) => {
   const templateVars = {
     email: req.cookies["email"],
   };
-  res.render("_login", templateVars);
+  return res.render("_login", templateVars);
 });
 
 //============================================= POST
 
 app.post("/urls", (req, res) => {
   const shortUrl = generateRandomString();
-  urlDatabase[shortUrl] = req.body.longURL;
-  res.redirect(`/urls/${shortUrl}`);
+  urlDatabase[shortUrl] = {
+    longURL: req.body.longURL,
+    userID: ""
+  };
+  return res.redirect(`/urls/${shortUrl}`);
 });
 
 app.post("/urls/:id/delete", (req, res) => {
-  delete urlDatabase[req.params.id];
-  res.redirect(`/urls`);
+  const loggedInUserID = req.cookies["user_id"];
+  if (loggedInUserID === urlDatabase[req.params.id]?.userID) {
+    delete urlDatabase[req.params.id];
+    return res.redirect("/urls");
+  } else {
+    return res.status(403);
+  }
+  
 });
 
 app.post("/urls/:id", (req, res) => {
-  console.log("here");
-  const shortUrl = req.params.id;
-  const updatedLongUrl = req.body.longURL;
-  console.log("updatedLongUrl ", updatedLongUrl);
-  urlDatabase[shortUrl] = updatedLongUrl;
-  res.redirect("/urls");
+  const shortUrl = req.params.id;                               
+  const updatedLongUrl = req.body.longURL;                               
+  // console.log("updatedLongUrl ", updatedLongUrl);
+  const loggedInUserID = req.cookies["user_id"];
+  if (loggedInUserID === urlDatabase[shortUrl].userID) {
+    urlDatabase[shortUrl].longURL = updatedLongUrl;                               
+    return res.redirect("/urls");
+  } else {
+    return res.status(403);
+  }
 });
 
 app.post("/login", (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
-
   // make sure email or password are not empty
   if (!email || !password) {
     return res.status(400).send(`no email and/or password was provided`);
@@ -133,23 +185,19 @@ app.post("/login", (req, res) => {
     if (user.email === email && user.password === password) {
       res.cookie("email", email);
       res.cookie("user_id", user.id);
-      res.redirect("/urls");
-    }
-    // if email is there but the password is incorrect then send back 403
-    else if (user.email === email && user.password !== password) {
-      res.status(403).send("email matches, password incorrect");
+      return res.redirect("/urls");
+    } else if (user.email === email && user.password !== password) {
+      return res.status(403); // if email is there but the password is incorrect then send back 403
     }
   }
-
-  res.status(403).send("email does not exist");
-
+  return res.status(403);
 });
 
 app.post("/logout", (req, res) => {
   const email = req.body.email;
   res.clearCookie("email");
   res.clearCookie("user_id");
-  res.redirect("/login");
+  return res.redirect("/login");
   // possibly put the js that is in the header file here one day
 
 });
