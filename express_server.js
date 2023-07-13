d640dcaf8bf081c2e74c2b231bb2eb7f6e66675d
jasminehilton <jasminehilton1@live.com>
2023-07-13 07:38:09 -0700

    log out is broken. cookie is the monster. sign in/logout/login work

--- express_server.js
+++ express_server.js
@@ -18,13 +18,11 @@ const urlDatabase = {
 const users = {
   userRandomID: {
     id: "userRandomID",
-    // username: "a123",                        //change this
     email: "a@a.com",
     password: "123",
   },
   user2RandomID: {
     id: "user2RandomID",
-    // username: "b456",                        //change this
     email: "b@b.com",
     password: "456",
   },
@@ -58,7 +56,6 @@ app.get("/hello", (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = {
     urls: urlDatabase,
-    // username: req.cookies["username"],                        //change this
     email: req.cookies["email"],
   };
   res.render("urls_index", templateVars);
@@ -66,7 +63,6 @@ app.get("/urls", (req, res) => {
 
 app.get("/urls/new", (req, res) => {
   const templateVars = {
-    // username: req.cookies["username"],                        //change this
     email: req.cookies["email"],
   };
   res.render("urls_new", templateVars);
@@ -86,12 +82,14 @@ app.get("/u/:id", (req, res) => {
 });
 
 app.get("/register", (req, res) => {
-  console.log(req.body);
   const templateVars = {
-    // username: req.cookies["username"],                        //change this
     email: req.cookies["email"],
   };
   res.render("_signup", templateVars);
+});
+
+app.get("/login", (req, res) => {
+  res.render("_login");
 });
 
 //============================================= POST
@@ -117,36 +115,31 @@ app.post("/urls/:id", (req, res) => {
 });
 
 app.post("/login", (req, res) => {
-  // const username = req.body.username;                        //change this
   const email = req.body.email;
-  // res.cookie("username", username);                        //change this
   res.cookie("email", email);
   res.redirect("/urls");
 });
 
 app.post("/logout", (req, res) => {
-  // res.clearCookie("username");                        //change this
-  res.clearCookie("email");
+  const email = req.body.email;
+  // res.clearCookie("email"); // this cookie is the problem!!! it is being cleared upon "logout" and then "email" is not defined. change it to userid maybe or some other identifier?
   res.redirect("/urls");
-// possibly put the js that is in the header file here one day
+  // possibly put the js that is in the header file here one day
+
 });
 
 app.post("/register", (req, res) => {
   const id = generateRandomString();
-  // const username = req.body.username;                        //change this
   const email = req.body.email;
   const password = req.body.password;
   
-  // console.log(email);
+  // make sure email or password are not empty
   if (!email || !password) {
     return res.status(400).send(`no email and/or password was provided`);
   }
 
-
-  let foundEmail = null;
-
+  // make sure that the email doesn't exist
   for (const userId in users) {
-    // console.log("email=",email, "users[userId].email=",users[userId].email, "users=",users, "userId=",userId);
     const user = users[userId];
     if (user.email === email) {
       console.log(' found a matching email');
@@ -155,59 +148,12 @@ app.post("/register", (req, res) => {
     }
   }
 
+  // add the unique user in the users list
   users[id] = {
     id: id,
-    // username: username,                        //change this
     email: email,
     password: password,
   };
-
-
-
   res.cookie("user_id", id);
   res.redirect("/urls",);
 });
-
-
-
-// // POST /login
-// app.post('/login', (req, res) => {
-//   // pull the data off the body object
-//   const username = req.body.username;
-//   const password = req.body.password;
-
-//   // did they NOT give us a username or password
-//   if (!username || !password) {
-//     res.status(400);
-//     return res.send('you must provide a username and password');
-//   }
-
-//   // look up the user based on their username
-//   let foundUser = null;
-
-//   for (const userId in users) {
-//     const user = users[userId];
-//     if (user.username === username) {
-//       // yay! we found our user!
-//       foundUser = user;
-//     }
-//   }
-
-//   // did we NOT find a user?
-//   if (!foundUser) {
-//     return res.status(400).send('no user with that username found');
-//   }
-
-//   // compare the passwords (do they NOT match)
-//   if (foundUser.password !== password) {
-//     return res.status(400).send('passwords do not match');
-//   }
-
-//   // happy path!!!
-//   // set a cookie
-//   res.cookie('userId', foundUser.id);
-
-//   // redirect the user somewhere
-//   res.redirect('/protected');
-// });
-
